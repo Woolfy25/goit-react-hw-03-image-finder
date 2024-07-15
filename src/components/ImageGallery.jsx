c0ecc7199981dfdee271487c2d4f6b7bee853f38
Ciutre Ramon <ramonciutre7@gmail.com>
2024-07-15 22:29:24 +0300

    update

---
+++ src/components/ImageGallery.jsx
@@ -0,0 +1,9 @@
+import css from './ImageGallery.module.css';
+
+class ImageGallery extends React.Component {
+  render() {
+    return <ul className={css.ImageGallery}></ul>;
+  }
+}
+
+export default ImageGallery;
