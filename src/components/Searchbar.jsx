8573331f580485aca8f0a4f8fe396291a86bf4d9
Ciutre Ramon <ramonciutre7@gmail.com>
2024-07-15 14:16:43 +0300

    start

---
+++ src/components/Searchbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Searchbar extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      test: '',
+    };
+  }
+  render() {
+    return <div>TESTING</div>;
+  }
+}
+
+export default Searchbar;
