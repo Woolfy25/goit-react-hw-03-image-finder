e7ce570700f58deba51b37f338c83b534d49ac22
Ciutre Ramon <ramonciutre7@gmail.com>
2024-07-15 20:20:21 +0300

    search

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,28 @@
+import React from 'react';
+import css from './App.module.css';
 import Searchbar from './Searchbar';
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      <Searchbar />
-    </div>
-  );
-};
+
+export class App extends React.Component {
+  handleValue = search => {
+    console.log('search', search);
+  };
+  render() {
+    return (
+      // <div
+      //   style={{
+      //     height: '100vh',
+      //     display: 'flex',
+      //     justifyContent: 'center',
+      //     alignItems: 'center',
+      //     fontSize: 40,
+      //     color: '#010101',
+      //   }}
+      // >
+      //   <Searchbar />
+      // </div>
+      <div className={css.App}>
+        <Searchbar onSubmit={this.handleValue} />
+      </div>
+    );
+  }
+}
--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,14 +1,43 @@
 import React from 'react';
+import css from './Searchbar.module.css';
 
 class Searchbar extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
-      test: '',
+      search: '',
     };
   }
+
+  handleSearch = event => {
+    this.setState({ search: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.onSubmit(this.state.search);
+  };
+
   render() {
-    return <div>TESTING</div>;
+    return (
+      <header className={css.Searchbar}>
+        <form className={css.SearchForm} onSubmit={this.handleSubmit}>
+          <button className={css.SearchFormButton} type="submit">
+            <span className={css.SearchFormButtonLabel}>Search</span>
+          </button>
+
+          <input
+            className={css.SearchFormInput}
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            // value={this.state.search}
+            onChange={this.handleSearch}
+          />
+        </form>
+      </header>
+    );
   }
 }
 
